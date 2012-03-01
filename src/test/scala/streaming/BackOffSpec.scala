d019aa9a5b187c13f36a05aed5c4c13db01b2269
Bryan J Swift <bryan.j.swift@gmail.com>
2012-03-01 00:36:24 -0500

    Change from object to class for Specs

--- src/test/scala/streaming/BackOffSpec.scala
+++ src/test/scala/streaming/BackOffSpec.scala
@@ -4,7 +4,7 @@ import org.scalatest.FunSpec
 import org.scalatest.BeforeAndAfter
 import org.scalatest.matchers.ShouldMatchers
 
-object BackOffSpec extends FunSpec with ShouldMatchers with BeforeAndAfter {
+class BackOffSpec extends FunSpec with ShouldMatchers with BeforeAndAfter {
   describe("Back Off strategy") {
 
     var backOff: BackOff = null
@@ -14,11 +14,13 @@ object BackOffSpec extends FunSpec with ShouldMatchers with BeforeAndAfter {
     }
 
     it("set the correct backOffTime and capBackOffAt") {
+      val backOff = BackOff(250, 16000)
       backOff.backOffTime should be (250)
       backOff.capBackOffAt should be (16000)
     }
 
     it("increment the back off time incrementally") {
+      val backOff = BackOff(250, 16000)
       backOff.backOff
       backOff.backOffTime should be (500)
       backOff.backOff
@@ -26,10 +28,11 @@ object BackOffSpec extends FunSpec with ShouldMatchers with BeforeAndAfter {
     }
 
     it("allow to reset the incremental back off time") {
+      val backOff = BackOff(250, 16000)
       backOff.backOff
       backOff.backOffTime should be (500)
       backOff.reset()
-      backOff.backOffTime should be (0)
+      backOff.backOffTime should be (backOff.origBackOffTime)
     }
   }
 }
--- src/test/scala/streaming/StreamingMethodsSpec.scala
+++ src/test/scala/streaming/StreamingMethodsSpec.scala
@@ -10,18 +10,12 @@ import org.apache.commons.httpclient._
 import org.apache.commons.httpclient.methods.PostMethod
 import org.apache.commons.httpclient.methods.GetMethod
 
-object StreamingMethodsSpec extends FunSpec with ShouldMatchers with BeforeAndAfter {
+class StreamingMethodsSpec extends FunSpec with ShouldMatchers with BeforeAndAfter {
   describe("Streaming Methods") {
 
-    var client: StreamingClientSpecHelper = null
-    var o: OutputStreamProcessor = null
-
-    before {
-      o = new OutputStreamProcessor
-      client = new StreamingClientSpecHelper("user", "password", o)
-    }
-
     it("build a Get request with specified params") {
+      val o = new OutputStreamProcessor
+      val client = new StreamingClientSpecHelper("user", "password", o)
       val params = new ArrayBuffer[String]
         params += "count=10"
       val request = client.buildGet("www.github.com", params)
@@ -29,6 +23,8 @@ object StreamingMethodsSpec extends FunSpec with ShouldMatchers with BeforeAndAf
     }
 
     it("build a post request with specified params") {
+      val o = new OutputStreamProcessor
+      val client = new StreamingClientSpecHelper("user", "password", o)
       val params = new ArrayBuffer[NameValuePair]
         params += new NameValuePair("follow", "cool,works,awesome")
       val request = client.buildPost("www.github.com", params)
